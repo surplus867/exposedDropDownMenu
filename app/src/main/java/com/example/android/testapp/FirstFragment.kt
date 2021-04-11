347e72cbebc0e5c7f38567328c27b1ab468b7985
Billy <yan.billy.20@gmail.com>
2021-04-10 22:21:03 -0700

    Exposed Drop-Down Menu- Forget about Spinner Tutorial

---
+++ app/src/main/java/com/example/android/testapp/FirstFragment.kt
@@ -0,0 +1,40 @@
+package com.example.android.testapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.android.testapp.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onResume() {
+        super.onResume()
+        val languages = resources.getStringArray(R.array.languages)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, languages)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+       _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
